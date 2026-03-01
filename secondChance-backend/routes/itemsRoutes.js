56b295100ea2678e7372e09c591b6a4230132b11
Aloshy Antony <aloshyantony639@gmail.com>
2026-03-01 23:23:06 +0530

    Create itemsRoutes.js

---
+++ secondChance-backend/routes/itemsRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const { connectToDatabase } = require("../models/db");
+const { ObjectId } = require("mongodb");
+
+// GET all items (Task 13)
+router.get("/", async (req, res) => {
+  const db = await connectToDatabase();
+  const items = await db.collection("items").find().toArray();
+  res.json(items);
+});
+
+// GET item by ID (Task 16)
+router.get("/:id", async (req, res) => {
+  const db = await connectToDatabase();
+  const item = await db.collection("items").findOne({
+    _id: new ObjectId(req.params.id)
+  });
+  res.json(item);
+});
+
+module.exports = router;
