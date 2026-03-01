5f9b505364935b0028a6cdf7129a00aa8f13c861
Aloshy Antony <aloshyantony639@gmail.com>
2026-03-02 00:24:51 +0530

    Update searchRoutes.js

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -2,16 +2,18 @@ const express = require("express");
 const router = express.Router();
 const { connectToDatabase } = require("../models/db");
 
-// Search by category
 router.get("/", async (req, res) => {
   try {
     const db = await connectToDatabase();
-    const { category } = req.query;
+    const { q } = req.query;
 
-    const filter = category ? { category } : {};
+    const filter = q
+      ? { title: { $regex: q, $options: "i" } }
+      : {};
 
     const results = await db.collection("items").find(filter).toArray();
     res.json(results);
+
   } catch (error) {
     res.status(500).json({ error: "Search failed" });
   }
