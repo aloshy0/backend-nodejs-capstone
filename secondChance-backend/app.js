6fc520850a68b477c91d797abd1d5a37073c45ed
Aloshy Antony <aloshyantony639@gmail.com>
2026-03-01 23:03:24 +0530

    Add root route to confirm backend is running

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -11,7 +11,9 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);  // 👈 USE AFTER CREATION
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
