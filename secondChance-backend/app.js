3fe3b931de94567fb1b35fed3357c404a6a50fd4
Aloshy Antony <aloshyantony639@gmail.com>
2026-03-02 00:30:22 +0530

    Add search routes to the app

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -9,6 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/search", require("./routes/searchRoutes"));
 
 // 👇 THIS IS CRITICAL
 app.use(express.static(path.join(__dirname, "public")));
